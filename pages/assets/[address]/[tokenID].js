def578cb5b2b2bea0e0020256527f6ea1ca2cc78
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-06-19 22:16:10 -0400

    got NFT JSON data to load

--- pages/assets/[address]/[tokenID].js
+++ pages/assets/[address]/[tokenID].js
@@ -12,21 +12,34 @@ const style = {
 
 const NFT = () => {
 
+    const [listing, setListing] = useState()
     const [loading, setLoading] = useState(false)
+    const router = useRouter()
+    const {tokenID} = router.query
+    console.log(tokenID)
 
     const marketplace = useMarketplace('0xCe9b05958ceB455A7937F9Ac3f21314689d7d70E')
     const address = useAddress()
 
+    useEffect(() => {
+        getListing()
+    }, [])
+
     const getListing = async () => {
         try {
             setLoading(true)
-            const listing = await marketplace.getListing()
+            const listing = await marketplace.getListing(BigNumber.from(tokenID))
+            console.log('running')
+            setListing(listing)
+            setLoading(false)
         } 
         
         catch(error) {
             console.log(error)
         }
     }
+
+    console.log(listing)
 
     return (
         <TopNavbarLayout>
