58a7eeaa171393f8c2f85f3790d05d7044c32c7a
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-06-20 03:57:02 -0400

    NFTSalesInfo styling

--- components/NFTDetails/NFTSalesInfo.js
+++ components/NFTDetails/NFTSalesInfo.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import {BsCreditCard2FrontFill, BsFillTagFill} from 'react-icons/bs'
+import {AiOutlineQuestionCircle, AiOutlineClockCircle} from 'react-icons/ai'
+import Image from 'next/image'
+
+const style = {
+    wrapper: ``
+}
 
 const NFTSalesInfo = ({price, buyNFT}) => {
-  return (
     console.log(price)
-    <div>NFTSalesInfo</div>
-  )
+    return (
+        <div className={style.wrapper}>
+            <div className={style.header}>
+                <div className={style.headerContent}></div>
+            </div>
+            
+            {/* <h2>{price}</h2>
+            <button onClick={buyNFT}>BUY NFT</button> */}
+        </div>
+    )
 }
 
 export default NFTSalesInfo
